c338bb7bd83f4cb72070892414593e64152ca8df
Adrian Padin <padin.adrian@gmail.com>
2023-08-10 22:53:49 -0400

    Cleaning up and adding stdin input

--- src/lib.rs
+++ src/lib.rs
@@ -3,32 +3,29 @@
 /* ===== Includes ===== */
 use std::fmt;
 
-
 /* ===== Global Data ===== */
 
 pub type Grid = Vec<Vec<char>>;
 
-
 /* ===== Structs ===== */
 
-#[derive(Copy, Clone)]
-#[derive(PartialEq, Eq)]
+/// 2D Coordinates
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Coordinates {
     pub x: usize,
-    pub y: usize
+    pub y: usize,
 }
 
-/* Print the Coordinates for testing/debugging. */
+/// Print the Coordinates for testing/debugging.
 impl fmt::Debug for Coordinates {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{{{}, {}}}", self.x, self.y)
     }
 }
 
-
 /* ===== Functions ===== */
 
-/* Find all valid neighbors of a given tile. */
+/// Find all valid neighbors of a given tile.
 pub fn find_neighbors(grid: &Grid, coords: Coordinates) -> Vec<Coordinates> {
     let mut neighbors = Vec::new();
     let width = grid.len();
@@ -36,41 +33,56 @@ pub fn find_neighbors(grid: &Grid, coords: Coordinates) -> Vec<Coordinates> {
     // Right
     if coords.x < (width - 1) {
         let (x, y) = (coords.x + 1, coords.y);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
     // Down-Right
     if coords.y < (width - 1) && coords.x < (width - 1) {
         let (x, y) = (coords.x + 1, coords.y + 1);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
     // Down
     if coords.y < (width - 1) {
         let (x, y) = (coords.x, coords.y + 1);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
     // Left
     if coords.x > 0 {
         let (x, y) = (coords.x - 1, coords.y);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
     // Up-Left
     if coords.x > 0 && coords.y > 0 {
         let (x, y) = (coords.x - 1, coords.y - 1);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
     // Up
     if coords.y > 0 {
         let (x, y) = (coords.x, coords.y - 1);
-        if grid[x][y] != '\0' { neighbors.push(Coordinates{x: x, y: y}); }
+        if grid[x][y] != '\0' {
+            neighbors.push(Coordinates { x, y });
+        }
     }
 
     neighbors
 }
 
-/* Found the list of coordinates that correspond to the given sequence of characters. */
-pub fn find_route(grid: &Grid, characters: &[char], mut start: Coordinates) ->
-    Result<Vec<Coordinates>, &'static str>
-{
+/// Found the list of coordinates that correspond to the given sequence of
+/// characters.
+pub fn find_route(
+    grid: &Grid,
+    characters: &[char],
+    mut start: Coordinates,
+) -> Result<Vec<Coordinates>, &'static str> {
     // First character must match starting point
     if characters[0] != grid[start.x][start.y] {
         return Err("Invalid start");
@@ -101,57 +113,75 @@ pub fn find_route(grid: &Grid, characters: &[char], mut start: Coordinates) ->
     Ok(route)
 }
 
-/* Find the tile in the grid that corresponds to the given letter. */
+/// Find the tile in the grid that corresponds to the given letter.
 pub fn find_tile(grid: &Grid, start_char: char) -> Option<Coordinates> {
     for x in 0..grid.len() {
         for y in 0..grid.len() {
             if start_char == grid[x][y] {
-                return Some(Coordinates{x: x, y: y});
+                return Some(Coordinates { x, y });
             }
         }
     }
     None
 }
 
-/* Functions to move around the grid. */
-fn move_right(c: &mut Coordinates) { c.x += 1; }
-fn move_down_right(c: &mut Coordinates) { c.x += 1; c.y += 1; }
-fn move_down(c: &mut Coordinates) { c.y += 1; }
-fn move_left(c: &mut Coordinates) { c.x -= 1; }
-fn move_up_left(c: &mut Coordinates) { c.x -= 1; c.y -= 1; }
-fn move_up(c: &mut Coordinates) { c.y -= 1; }
+fn move_right(c: &mut Coordinates) {
+    c.x += 1;
+}
+fn move_down_right(c: &mut Coordinates) {
+    c.x += 1;
+    c.y += 1;
+}
+fn move_down(c: &mut Coordinates) {
+    c.y += 1;
+}
+fn move_left(c: &mut Coordinates) {
+    c.x -= 1;
+}
+fn move_up_left(c: &mut Coordinates) {
+    c.x -= 1;
+    c.y -= 1;
+}
+fn move_up(c: &mut Coordinates) {
+    c.y -= 1;
+}
 
-/* Find the inverse Fibonacci (kind of). */
+/// Find the inverse Fibonacci (kind of).
 fn inv_fibish(mut f_n: usize) -> usize {
     let mut n = 0;
-    while f_n > n { f_n -= n; n += 1 }
+    while f_n > n {
+        f_n -= n;
+        n += 1
+    }
     n
 }
 
-/* Given the length of the input string, return the number of layers required.
- * For example:
- * 1 => 1 layer
- * 2-7 => 2 layers
- * 8-19 => 3 layers, etc.
- */
+/// Given the length of the input string, return the number of layers required.
+///
+/// For example:
+/// * 1 => 1 layer
+/// * 2-7 => 2 layers
+/// * 8-19 => 3 layers, etc.
+///
 fn layers_n(i: usize) -> usize {
-    if i < 2 { 1 }
-    else {
+    if i < 2 {
+        1
+    } else {
         inv_fibish(((i - 2) / 6) + 1) + 1
     }
 }
 
-/* Given the length of the input string, returns the required width of the grid. */
+/// Given the length of the input string, returns the required width of the grid.
 fn grid_width(i: usize) -> usize {
-    if i < 1 { 1 }
-    else {
+    if i < 1 {
+        1
+    } else {
         ((layers_n(i)) * 2) - 1
     }
 }
 
-/* Fill out the grid given the sequence of characters. */
+/// Fill out the grid given the sequence of characters.
 pub fn construct_grid(letters: &[char]) -> Result<Grid, &'static str> {
-
     // Find width of grid
     let width: usize = grid_width(letters.len());
     let mut grid: Grid = vec![vec!['\0'; width]; width];
@@ -161,7 +191,10 @@ pub fn construct_grid(letters: &[char]) -> Result<Grid, &'static str> {
     // Find the center
     let mut start = {
         let center = width / 2;
-        Coordinates{x: center, y: center}
+        Coordinates {
+            x: center,
+            y: center,
+        }
     };
 
     println!("Center: {:?}", start);
@@ -175,55 +208,91 @@ pub fn construct_grid(letters: &[char]) -> Result<Grid, &'static str> {
     let mut layer = 1;
     let mut n = 1;
     'outer: while n < letters.len() {
-        // Move to the right to start a new layer
+        // Move to the right to start a new layer.
         move_right(&mut start);
         grid[start.x][start.y] = letters[n];
-        n += 1; if n >= letters.len() { break; }
+        n += 1;
+        if n >= letters.len() {
+            break;
+        }
 
-        // Move down-right by `layer-1` (why -1? because we already moved right 1 to start the new layer)
-        for _ in 0..(layer-1) {
+        // Move down-right by `layer-1` (why -1? because we already moved
+        // right 1 to start the new layer).
+        for _ in 0..(layer - 1) {
             move_down_right(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         // Move down by `layer`
         for _ in 0..layer {
             move_down(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         // Move left by `layer`
         for _ in 0..layer {
             move_left(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         // Move up-left by `layer`
         for _ in 0..layer {
             move_up_left(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         // Move up by `layer`
         for _ in 0..layer {
             move_up(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         // Move right by `layer`
         for _ in 0..layer {
             move_right(&mut start);
             grid[start.x][start.y] = letters[n];
-            n += 1; if n >= letters.len() { break 'outer; }
+            n += 1;
+            if n >= letters.len() {
+                break 'outer;
+            }
         }
 
         layer += 1;
     }
 
     Ok(grid)
+}
+
+/// Print the grid
+pub fn print_grid(grid: &Grid) {
+    for y in 0..grid.len() {
+        for x in 0..grid.len() {
+            if grid[x][y] > '\0' {
+                print!("  {}", grid[x][y]);
+            } else {
+                print!("   ");
+            }
+        }
+        println!("");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,50 @@
 /* ===== main.rs ===== */
 
 /* ===== Includes ===== */
-use hex_grid::{
-    Grid,
-    Coordinates,
-    find_neighbors,
-    find_route,
-    find_tile,
-    construct_grid,
-};
-
-
-/* ===== Functions ===== */
-
-/* Find and print the neighbors of the given coordinates. */
-fn print_neighbors(grid: &Grid, coords: Coordinates) {
-    let neighbors = find_neighbors(grid, coords);
-    println!("Neighbors found: {:?}", neighbors.len());
-    let mut neighbor_letters = Vec::new();
-    for it in &neighbors {
-        neighbor_letters.push(grid[it.x][it.y]);
-    }
-    println!("Found neighbors: {:?}", neighbor_letters);
-}
-
+use hex_grid::{construct_grid, find_route, find_tile, print_grid, Grid};
+use std::env;
 
 /* ===== MAIN ===== */
 
 fn main() {
-    // TODO: Take args from input
-    let grid_input = "ABCDEFGHIJKLMNOPQRS";
-    let expected_route = "ABCD";
+    // Get args from stdin
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        x if x < 3 => {
+            println!("Too few arguments: {:?}", args);
+            return;
+        }
+        x if x > 3 => {
+            println!("Too few arguments: {:?}", args);
+            return;
+        }
+        _ => {}
+    }
+    let grid_input = &args[1];
+    let expected_route = &args[2];
 
-    // let mut grid = [['\0'; SIZE]; SIZE];
-
+    // Construct grid
+    println!("Building grid from {}", grid_input);
     let letters: Vec<char> = grid_input.chars().collect();
     let grid: Grid = construct_grid(&letters).expect("Could not construct grid.");
 
-    println!("Finding neighbors of A...");
-    print_neighbors(&grid, Coordinates{x: 2, y: 2});
+    println!("Grid:");
+    print_grid(&grid);
 
-    println!("Finding neighbors of B...");
-    print_neighbors(&grid, Coordinates{x: 3, y: 2});
+    // Find route
+    let start_char = expected_route.chars().nth(0).unwrap();
+    let start = find_tile(&grid, start_char);
+    if start.is_none() {
+        println!("Could not find starting point for {}", start_char);
+        return;
+    } else {
+        println!("Starting point: {:?}", start.unwrap());
+    }
 
-    println!("Finding neighbors of E...");
-    print_neighbors(&grid, Coordinates{x: 1, y: 2});
-
-    println!("Finding neighbors of S...");
-    print_neighbors(&grid, Coordinates{x: 2, y: 0});
-
-    println!("Finding route for ABCD");
+    println!("Finding route for {}", expected_route);
     let chars: Vec<char> = expected_route.chars().collect();
-    match find_route(&grid, &chars, Coordinates{x: 2, y: 2}) {
+    match find_route(&grid, &chars, start.unwrap()) {
         Ok(v) => println!("Found route: {:?}", v),
-        Err(e) => println!("Route not found: {:?}", e)
-    }
-
-    println!("Finding route for ABHSRQPE");
-    let chars: Vec<char> = "ABHSRQPE".chars().collect();
-    match find_route(&grid, &chars, Coordinates{x: 2, y: 2}) {
-        Ok(v) => println!("Found route: {:?}", v),
-        Err(e) => println!("Route not found: {:?}", e)
-    }
-
-    println!("Finding route for AENDVML");
-    let chars: Vec<char> = "AENDVML".chars().collect();
-    match find_route(&grid, &chars, Coordinates{x: 2, y: 2}) {
-        Ok(v) => println!("Found route: {:?}", v),
-        Err(e) => println!("{}", e)
-    }
-
-    println!("Finding start position for A");
-    match find_tile(&grid, 'A') {
-        Some(coords) => println!("Found tile: {:?}", coords),
-        None => println!("Tile not found.")
-    }
-
-    println!("Finding start position for D");
-    match find_tile(&grid, 'D') {
-        Some(coords) => println!("Found tile: {:?}", coords),
-        None => println!("Tile not found.")
-    }
-
-    println!("Finding start position for K");
-    match find_tile(&grid, 'K') {
-        Some(coords) => println!("Found tile: {:?}", coords),
-        None => println!("Tile not found.")
-    }
-
-    println!("Finding start position for Q");
-    match find_tile(&grid, 'Q') {
-        Some(coords) => println!("Found tile: {:?}", coords),
-        None => println!("Tile not found.")
-    }
-
-    println!("Finding start position for Z");
-    match find_tile(&grid, 'Z') {
-        Some(coords) => println!("Found tile: {:?}", coords),
-        None => println!("Tile not found.")
+        Err(_) => println!("Route not found."),
     }
 }
-
